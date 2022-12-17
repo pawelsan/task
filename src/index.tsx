083d3d7a39e9ed8bc954995645470a019ed1652d
Paweł <pawelsan@yahoo.com>
2022-12-17 22:49:16 +0000

    changes related to codesandbox errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React, { ReactElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-import { API_URL } from 'constants/'
-import 'styles/index.scss'
+import { API_URL } from './constants/'
+import './styles/index.scss'
 
 const apiClient = new ApolloClient({
   uri: API_URL,
